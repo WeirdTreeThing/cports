ea02dd1fe0131940b1c3fdbf9a5a26ff35730c09
WeirdTreeThing <bradyn127@protonmail.com>
2025-03-30 09:21:55 -0400

    main/linux-sc7180: new package

---
+++ main/linux-sc7180/template.py
@@ -0,0 +1,53 @@
+pkgname = "linux-sc7180"
+pkgver = "6.6.28"
+pkgrel = 0
+archs = ["aarch64"]
+build_style = "linux-kernel"
+configure_args = ["FLAVOR=sc7180", f"RELEASE={pkgrel}"]
+make_dir = "build"
+hostmakedepends = ["base-kernel-devel"]
+depends = ["base-kernel"]
+provides = ["linux"]
+pkgdesc = f"Linux kernel {pkgver[0 : pkgver.rfind('.')]}.x for SC7180 Chromebooks"
+license = "GPL-2.0-only"
+url = "https://kernel.org"
+source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
+sha256 = "818716ed13e7dba6aaeae24e3073993e260812ed128d10272e94b922ee6d3394"
+# no meaningful checking to be done
+options = [
+    "!check",
+    "!debug",
+    "!strip",
+    "!scanrundeps",
+    "!scanshlibs",
+    "!lto",
+    "textrels",
+    "execstack",
+    "foreignelf",  # vdso32
+]
+
+if self.current_target == "custom:generate-configs":
+    hostmakedepends += ["base-cross", "ncurses-devel"]
+
+if self.profile().cross:
+    broken = "linux-devel does not come out right"
+
+
+@subpackage("linux-sc7180-devel")
+def _(self):
+    self.depends += ["clang"]
+    self.options = ["foreignelf", "execstack", "!scanshlibs"]
+    return ["usr/src", "usr/lib/modules/*/build"]
+
+
+@subpackage("linux-sc7180-dbg", self.build_dbg)
+def _(self):
+    self.options = [
+        "!scanrundeps",
+        "!strip",
+        "!scanshlibs",
+        "foreignelf",
+        "execstack",
+        "textrels",
+    ]
+    return ["usr/lib/debug", "usr/lib/modules/*/apk-dist/boot/System.map-*"]
