5f3228580f96fa76803d1ddc20137cf613ab5f62
WeirdTreeThing <bradyn127@protonmail.com>
2025-03-30 09:22:23 -0400

    main/linux-mt8192: new package

---
+++ main/linux-mt8192/template.py
@@ -0,0 +1,53 @@
+pkgname = "linux-mt8192"
+pkgver = "6.10.14"
+pkgrel = 0
+archs = ["aarch64"]
+build_style = "linux-kernel"
+configure_args = ["FLAVOR=mt8192", f"RELEASE={pkgrel}"]
+make_dir = "build"
+hostmakedepends = ["base-kernel-devel"]
+depends = ["base-kernel"]
+provides = ["linux"]
+pkgdesc = f"Linux kernel {pkgver[0 : pkgver.rfind('.')]}.x for MT8192 Chromebooks"
+license = "GPL-2.0-only"
+url = "https://kernel.org"
+source = f"https://cdn.kernel.org/pub/linux/kernel/v{pkgver[0]}.x/linux-{pkgver}.tar.xz"
+sha256 = "55e5bcbc68d66776fc46896291f0a24844beb57817345a854d65e3d055fa423e"
+# no meaningful checking to be done
+options = [
+    "!check",
+    "!debug",
+    "!strip",
+    "!scanrundeps",
+    "!scanshlibs",
+    "!lto",
+    "textrels",
+    "execstack",
+    "foreignelf",  # vdso32
+]
+
+if self.current_target == "custom:generate-configs":
+    hostmakedepends += ["base-cross", "ncurses-devel"]
+
+if self.profile().cross:
+    broken = "linux-devel does not come out right"
+
+
+@subpackage("linux-mt8192-devel")
+def _(self):
+    self.depends += ["clang"]
+    self.options = ["foreignelf", "execstack", "!scanshlibs"]
+    return ["usr/src", "usr/lib/modules/*/build"]
+
+
+@subpackage("linux-mt8192-dbg", self.build_dbg)
+def _(self):
+    self.options = [
+        "!scanrundeps",
+        "!strip",
+        "!scanshlibs",
+        "foreignelf",
+        "execstack",
+        "textrels",
+    ]
+    return ["usr/lib/debug", "usr/lib/modules/*/apk-dist/boot/System.map-*"]
